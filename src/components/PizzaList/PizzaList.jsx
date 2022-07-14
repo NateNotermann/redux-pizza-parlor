1e9e2a8717d3795bd4f220b23c9c544a12be185f
Rachel Bruce <rachelbruce922@gmail.com>
2022-07-14 17:00:08 -0500

    created addToCart

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 
@@ -5,6 +7,16 @@ import { useDispatch, useSelector } from 'react-redux';
 function PizzaList() {
 
 const pizzaList = useSelector(store => store.pizzaList);
+
+const [cartList, setCartList] = useState([]);
+console.log('cartList', cartList)
+
+const addToCart = (pizza) => {
+console.log('pizza in addToCart', pizza);
+setCartList([...cartList, pizza]);
+console.log('cartList in addToCart', cartList)
+
+}
 
 
     return(
@@ -16,7 +28,10 @@ const pizzaList = useSelector(store => store.pizzaList);
                     <h4>{pizza.name}</h4>
                     <p>{pizza.description}</p>
                     <h5>${pizza.price}</h5>
-                    <button>Add</button>
+                    <button key={pizza.id} onClick={(event) => addToCart(pizza)} >Add</button>
+
+                    {/* <button key={pizza.id} onClick={() => setCartList(pizza)}>Add</button> */}
+
                     </div>
                 )
             })}
